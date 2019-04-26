00e00cb279a6e9837dcc44802f96c657a0a4d4fe
barta.gergo <barta.gergo@gammadigital.hu>
2019-04-26 15:22:18 +0200

    Add colors message color meta color extra colors

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 'use strict';
 
-var stream = require('stream');
-var util = require('util');
-var formatRecord = require('./lib/format-record');
-var xtend = require('xtend');
+const stream = require('stream');
+const util = require('util');
+const formatRecord = require('./lib/format-record');
+const xtend = require('xtend');
 
-
-var Writable = stream.Writable;
+const { Writable } = stream;
 
 module.exports = BunyanFormatWritable;
 
@@ -14,45 +13,49 @@ util.inherits(BunyanFormatWritable, Writable);
 
 /**
  * Creates a writable stream that formats bunyan records written to it.
- * 
+ *
  * @name BunyanFormatWritable
  * @function
  * @param opts {Options} passed to bunyan format function
  *  - outputMode: short|long|simple|json|bunyan
  *  - color (true): toggles colors in output
  *  - colorFromLevel: allows overriding log level colors
- * @param out {Stream} (process.stdout) writable stream to write 
+ * @param out {Stream} (process.stdout) writable stream to write
  * @return {WritableStream} that you can pipe bunyan output into
  */
-function BunyanFormatWritable (opts, out) {
-  if (!(this instanceof BunyanFormatWritable)) return new BunyanFormatWritable(opts, out);
+function BunyanFormatWritable(opts, out) {
+	if (!(this instanceof BunyanFormatWritable)) return new BunyanFormatWritable(opts, out);
 
-  opts = opts || {};
-  opts.objectMode = true;
-  Writable.call(this, opts);
+	opts = opts || {};
+	opts.objectMode = true;
+	Writable.call(this, opts);
 
-  this.opts = xtend({ 
-    outputMode: 'short', 
-    color: true,
-    colorFromLevel: {
-      10: 'brightBlack',    // TRACE
-      20: 'brightBlack',    // DEBUG
-      30: 'green',          // INFO
-      40: 'magenta',        // WARN
-      50: 'red',            // ERROR
-      60: 'brightRed',      // FATAL
-    }
-  }, opts);
-  this.out = out || process.stdout;
+	this.opts = xtend({
+		outputMode: 'short',
+		color: true,
+		msgColor: 'cyan',
+		timeColor: 'brightWhite',
+		metaColor: 'brightRed',
+		extraColor: 'brightCyan',
+		colorFromLevel: {
+			10: 'brightBlack',    // TRACE
+			20: 'brightYellow',    // DEBUG
+			30: 'brightGreen',          // INFO
+			40: 'brightMagenta',        // WARN
+			50: 'red',            // ERROR
+			60: 'brightRed',      // FATAL
+		},
+	}, opts);
+	this.out = out || process.stdout;
 }
 
-BunyanFormatWritable.prototype._write = function (chunk, encoding, cb) {
-  var rec;
-  try { 
-    rec = JSON.parse(chunk);
-    this.out.write(formatRecord(rec, this.opts));
-  } catch (e) {
-    this.out.write(chunk);
-  }
-  cb();
+BunyanFormatWritable.prototype._write = function(chunk, encoding, cb) {
+	let rec;
+	try {
+		rec = JSON.parse(chunk);
+		this.out.write(formatRecord(rec, this.opts));
+	} catch (e) {
+		this.out.write(chunk);
+	}
+	cb();
 };
